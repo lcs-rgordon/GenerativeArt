4c8f5d656918aa5296864c15c135f14007807ab4
Russell Gordon <rgordon@lcs.on.ca>
2023-10-24 09:25:51 -0400

    Finished logic for remaining triangle fill colour.

--- GenerativeArt/Views/ContentView.swift
+++ GenerativeArt/Views/ContentView.swift
@@ -7,26 +7,41 @@
 
 import SwiftUI
 
+// Implementation based on rules given here:
+//
+// https://ics4u-2023-24.russellgordon.ca/images/introduction-to-generative-art/
+
 struct ContentView: View {
-    
+
     // MARK: Stored properties
     
     // Decide on colours
-    let colourDecision = Coin.flip()
+    let flipForColor = Coin.heads
     let colourOne = Color.red
     let colourTwo = Color.blue
     
     // Decisions for rest of the design
-    let flipOne = Coin.tails    // Focus on first form for now
+    let flipOne = Coin.flip()
     let flipTwo = Coin.flip()
     let flipThree = Coin.flip()
     
     // MARK: Computed properties
     var markerOne: Color {
-        return colourDecision == .heads ? colourOne : colourTwo
+        return flipForColor == .heads ? colourOne : colourTwo
     }
     var markerTwo: Color {
-        return colourDecision == .tails ? colourTwo : colourOne
+        return flipForColor == .heads ? colourTwo : colourOne
+    }
+    
+    // Work out remaining fill color
+    var remainingTriangleFillColor: Color {
+        
+        if flipThree == .heads {
+            return markerTwo
+        } else {
+            return .clear
+        }
+            
     }
     
     var body: some View {
@@ -37,13 +52,13 @@ struct ContentView: View {
                 // Top left to bottom right
                 ZStack {
                     
-                    TriangleTopLeft()
+                    TriangleTopRight()
                         .stroke(.black)
-                        .fill(flipTwo == .heads ? markerOne : .clear)
+                        .fill(flipTwo == .heads ? markerOne : remainingTriangleFillColor)
 
-                    TriangleBottomRight()
+                    TriangleBottomLeft()
                         .stroke(.black)
-                        .fill(flipTwo == .tails ? markerOne : .clear)
+                        .fill(flipTwo == .tails ? markerOne : remainingTriangleFillColor)
 
                 }
                 .frame(width: 100, height: 100)
@@ -53,13 +68,13 @@ struct ContentView: View {
                 // Top right to bottom left
                 ZStack {
                     
-                    TriangleTopRight()
+                    TriangleTopLeft()
                         .stroke(.black)
-                        .fill(flipTwo == .heads ? markerOne : .clear)
+                        .fill(flipTwo == .heads ? markerOne : remainingTriangleFillColor)
 
-                    TriangleBottomLeft()
+                    TriangleBottomRight()
                         .stroke(.black)
-                        .fill(flipTwo == .tails ? markerOne : .clear)
+                        .fill(flipTwo == .tails ? markerOne : remainingTriangleFillColor)
 
                 }
                 .frame(width: 100, height: 100)
